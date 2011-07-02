d84aac939544ba8adb6bd9a6782ea1ef390b0951
aalm <andrew.alm@gettyimages.com>
2011-07-02 14:18:19 -0700

    Added missing files.

---
+++ src/Assman/ResourceModeCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Assman
+{
+    public enum ResourceModeCondition
+    {
+        Never,
+        DebugOnly,
+        ReleaseOnly,
+        Always
+    }
+
+    public static class ResourceModeConditionExtensions
+    {
+        public static bool IsTrue(this ResourceModeCondition resourceModeCondition, ResourceMode resourceMode)
+        {
+            switch (resourceModeCondition)
+            {
+                case ResourceModeCondition.Never:
+                    return false;
+                case ResourceModeCondition.DebugOnly:
+                    return resourceMode == ResourceMode.Debug;
+                case ResourceModeCondition.ReleaseOnly:
+                    return resourceMode == ResourceMode.Release;
+                case ResourceModeCondition.Always:
+                    return true;
+                default:
+                    throw new ArgumentOutOfRangeException("resourceModeCondition");
+            }
+        }
+
+        public static bool IsFalse(this ResourceModeCondition resourceModeCondition, ResourceMode resourceMode)
+        {
+            return !resourceModeCondition.IsTrue(resourceMode);
+        }
+    }
+}
