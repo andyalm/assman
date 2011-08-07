8d84b91af0ffd67ac3153ac2f1b0c9bda555bd0c
aalm <andrew.alm@gettyimages.com>
2011-08-06 23:13:26 -0700

    Updated the SassContentFilter to use the ProcessContent method instead of ProcessFileContent.  ProcessContent is not actually supported, but I am going to submit a pull request. For now, the checked in version of SassAndCoffee.Core is not a released version.

---
+++ src/Assman.SassAndCoffee/SassContentFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+using Assman.ContentFiltering;
+
+using SassAndCoffee.Core.Compilers;
+
+namespace Assman.SassAndCoffee
+{
+    public class SassContentFilter : IContentFilter
+    {
+        private readonly SassFileCompiler _compiler;
+
+        public SassContentFilter(SassFileCompiler compiler)
+        {
+            _compiler = compiler;
+        }
+
+        public string FilterContent(string content, ContentFilterContext context)
+        {
+            return _compiler.ProcessContent(content, Path.GetExtension(context.ResourceVirtualPath));
+        }
+    }
+}
