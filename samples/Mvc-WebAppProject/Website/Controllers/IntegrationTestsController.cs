c5d82b458c96410eceb24a551b892c32956246ef
aalm <andrew.alm@gettyimages.com>
2011-08-11 20:52:34 -0700

    Fixed a bug where resources registerd via a RenderAction call would not register themselves in the parent view.

--- samples/Mvc-WebAppProject/Website/Controllers/IntegrationTestsController.cs
+++ samples/Mvc-WebAppProject/Website/Controllers/IntegrationTestsController.cs
@@ -2,11 +2,16 @@ using System.Web.Mvc;
 
 namespace Website.Controllers
 {
-	public class IntegrationTestsController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class IntegrationTestsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViaRenderAction()
+        {
+            return PartialView();
+        }
+    }
 }
--- src/Assman.Mvc/ViewContextExtensions.cs
+++ src/Assman.Mvc/ViewContextExtensions.cs
@@ -5,19 +5,22 @@ using Assman.Registration;
 
 namespace Assman.Mvc
 {
-	public static class ViewContextExtensions
-	{
-		public static IResourceRegistryAccessor ResourceRegistries(this ViewContext viewContext)
-		{
-			var key = "__ResourceRegistries" + viewContext.View.GetHashCode();
-			var resourceRegistries = viewContext.HttpContext.Items[key] as IResourceRegistryAccessor;
-			if (resourceRegistries == null)
-			{
-				resourceRegistries = new GenericResourceRegistryAccessor().UseConsolidation();
-				viewContext.HttpContext.Items[key] = resourceRegistries;
-			}
+    public static class ViewContextExtensions
+    {
+        public static IResourceRegistryAccessor ResourceRegistries(this ViewContext viewContext)
+        {
+            //generate the key off of the CurrentExecutionFilePath.  This should stay the same unless a Server.Transfer happens, in which
+            //case, it will change and we will want to start clean anyways.  Originally we used the HashCode of the current view, but that
+            //broke RenderAction.
+            var key = "__ResourceRegistries" + viewContext.HttpContext.Request.CurrentExecutionFilePath;
+            var resourceRegistries = viewContext.HttpContext.Items[key] as IResourceRegistryAccessor;
+            if (resourceRegistries == null)
+            {
+                resourceRegistries = new GenericResourceRegistryAccessor().UseConsolidation();
+                viewContext.HttpContext.Items[key] = resourceRegistries;
+            }
 
-			return resourceRegistries;
-		}
-	}
+            return resourceRegistries;
+        }
+    }
 }
