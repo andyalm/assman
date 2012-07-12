25dfb8251599d55f896137be97c2485a5da7fd5b
Andy Alm <bogus@bogus.com>
2012-07-11 20:13:57 -0700

    Added config flags to disable the automatic configuration of routes.

--- src/Assman.Mvc/IgnoreRoutesForConsolidatedUrls.cs
+++ src/Assman.Mvc/IgnoreRoutesForConsolidatedUrls.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -17,7 +20,26 @@ namespace Assman.Mvc
 
         public static void IgnoreWebResourceRoutes(this RouteCollection routes)
         {
-            routes.IgnoreRoute("{*consolidatedurls}", new { consolidatedurls = @".*(\.js|\.css)(/.*)?" });   
+            var autoIgnoreJsRoutes = ConfigurationManager.AppSettings.AsBool("Assman.AutoIgnoreJsRoutes", false);
+            if(!autoIgnoreJsRoutes)
+                routes.IgnoreRoute("{*scripturls}", new { scripturls = @".*(\.js)(/.*)?" });
+
+            var autoIgnoreCssRoutes = ConfigurationManager.AppSettings.AsBool("Assman.AutoIgnoreCssRoutes", false);
+            if (!autoIgnoreCssRoutes)
+                routes.IgnoreRoute("{*stylesheeturls}", new { stylesheeturls = @".*(\.css)(/.*)?" });
+        }
+
+        private static bool AsBool(this NameValueCollection appSettings, string key, bool defaultValue)
+        {
+            var strValue = appSettings[key];
+            if (strValue == null)
+                return defaultValue;
+
+            bool bValue;
+            if (Boolean.TryParse(strValue, out bValue))
+                return bValue;
+            else
+                return defaultValue;
         }
 	}
 }
